fb0101baa8e95af02cdf594afe1d5c3c579c130e
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-08-26 11:23:30 +0530

    first commit

---
+++ products/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home Page")
+
+
+def about(request):
+    return HttpResponse("About Page")
+
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+
+
+
+
+
+
