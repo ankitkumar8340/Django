b0c0ba01aac6201c078934c3069565b5f05f2a59
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-09-09 00:12:03 +0530

    completed dynamic and static urls

--- New-DJango/products/urls.py
+++ New-DJango/products/urls.py
@@ -7,5 +7,6 @@ urlpatterns =[
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('thing/<str:product_desc>/', views.product_description, name='product_description'),
     re_path(r'^article/(?P<year>[0-9]{4})/$', views.article_by_year, name='article_by_year'),
+    path('profile/<str:name>/', views.name, name='name')
     
 ]
--- New-DJango/products/views.py
+++ New-DJango/products/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import Http404
 
 def hello(request):
     return HttpResponse('hello this is my first django views')
 
-def product_detail(request, product_id):
-    return HttpResponse(f"you are viewing product #{product_id}")
+# def product_detail(request, product_id):
+#     return HttpResponse(f"you are viewing product #{product_id}")
 
 def product_description(request, product_desc):
     return HttpResponse(f"you are viewing product #{product_desc}")
@@ -16,5 +17,12 @@ def product_description(request, product_desc):
 def article_by_year(request, year):
     return HttpResponse(f"You are viewing request_date #{year}")
 
+def product_detail(request, product_id):
+    products = {1: "Laptop", 2: "Phone"}
+    if product_id not in products:
+        raise Http404("Product not found")
+    return HttpResponse(products[product_id])
 
+def name(request, name):
+    return HttpResponse(f"Hello how are you..?? my name is #{name}")
 
